b2cfcee21b6b26f1bf4f30c1b63cd9773ee85e5d
a-wyrm <86126872+a-wyrm@users.noreply.github.com>
2021-07-06 15:50:10 -0400

    Add files via upload

--- Olm_main.py
+++ Olm_main.py
@@ -6,18 +6,33 @@ language = 'en'
 # eventually, the program name will be user-entered
 program_name = 'Olm_t.py'
 
+# Eventually, this program will be able to tell the edits to a code
+# and give the user the option of starting the TTS at the specific thing
+# they edited
+
 if (os.path.isfile('Olm_wf.txt') == True):
     os.remove('Olm_wf.txt')
 
+# eventually, I will make an interface that will allow these properties
+# to be turned off
+program_t = True
+
 with open(program_name,'r') as tts_file, open('Olm_wf.txt','a') as write_f:
-    
+    # Program line counter
+    program_count = 1
+
     for line in tts_file:
         # NEXT THING TO IMPLEMENT:
-        # PROGRAM LINE COUNTER, IGNORE COMMENTS
+        # IGNORE COMMENTS
         # LIST ARRAY PROPERTIES, LIST VARIABLE PROPERTIES
-        # eventually, I will make an interface that will allow these properties to be
-        # turned off
-        # Find a way to delay indents
+        if (program_t == True) and line != '\n':
+            write_f.write(str(program_count) + '... ')
+            program_count += 1
+        # This is how we delay indents, we do this by tricking the tokenizer with "..."
+        # this delays it just enough to make the program comprehendable
+        if '\n' in line:
+            line = ("... ") + line
+
         write_f.write(line)
 
 
