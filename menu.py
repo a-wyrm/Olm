2fd354782e1c7bcd522dae90466aadbead50cc9f
a-wyrm <86126872+a-wyrm@users.noreply.github.com>
2021-07-21 00:11:28 -0400

    Add files via upload

--- menu.py
+++ menu.py
@@ -18,10 +18,19 @@ def btn_clicked():
 def trans_clicked():
     playsound('./transcribe_text.mp3')
 
+
+def make_x(image):
+    image['image'] = img2
+    print('clicked')
+    #if(slow_flag):
+    #elif(slow_flag == 1):
+
+
 window = Tk()
 window.title("Olm")
 window.geometry("870x580")
 window.configure(bg = "#b98b97")
+
 
 canvas = Canvas(
     window,
@@ -42,7 +51,7 @@ title.place(x = 27.0, y = 25.0)
 info_text = Label(text="Olm is an application used to transcribe\n"
                    "Python programs into text-to-speech.\n"
                    "On the right side of this window,\n"
-                   "there are four buttons and a text field.\n",
+                   "there are four buttons and a select field.\n",
                   bg = "#ebebeb", fg = "#b98b97",
                   justify = "left",
                   font = ("Georgia", int(16.5)))
@@ -56,7 +65,7 @@ instructions_text = Label(text="The first button will ask if you want the text\n
                           "The fourth button will transcribe variable properties\n"
                           "(such as the type of variable, or array size).\n\n"
                           "Enter your program path into the select field,\n"
-                          "then generate the TSS at the very bottom.",
+                          "then generate the TTS at the very bottom.",
                   bg = "#ebebeb", fg = "#b98b97",
                   justify = "left",
                   font = ("Georgia", int(13.5)))
@@ -73,26 +82,60 @@ trans_btn.place(x = 30, y = 480, width = 380, height = 70)
 
 
 
-
+img2 = PhotoImage(file="./images/button_true.png")
 slower_text = Label(text = "Slower text?",
                     bg = "#b98b97", fg = "#000000",
                     font = ("Georgia", int(37.0)))
 slower_text.place(x = 550.0, y = 30.0)
+slow_not_btn_img = PhotoImage(file="./images/button_not.png")
+slow_not_btn = Button(image = slow_not_btn_img,
+                      borderwidth = 0,
+                      highlightthickness = 0,
+                      command = make_x,
+                      relief = "flat")
+slow_not_btn.place(x = 440, y = 5, width = 110, height = 90)
+slow_not_btn['command'] = lambda arg=slow_not_btn:make_x(arg)
 
 counter_text = Label(text = "Counter?",
                     bg = "#b98b97", fg = "#000000",
                     font = ("Georgia", int(37.0)))
 counter_text.place(x = 550.0, y = 120.0)
+counter_not_btn_img = PhotoImage(file="./images/button_not.png")
+counter_not_btn = Button(image = counter_not_btn_img,
+                      borderwidth = 0,
+                      highlightthickness = 0,
+                      command = make_x,
+                      relief = "flat")
+counter_not_btn.place(x = 440, y = 95, width = 110, height = 90)
+counter_not_btn['command'] = lambda arg=counter_not_btn:make_x(arg)
+
+
 
 comments_text = Label(text = "Comments?",
                     bg = "#b98b97", fg = "#000000",
                     font = ("Georgia", int(37.0)))
 comments_text.place(x = 550.0, y = 210.0)
+comments_not_btn_img = PhotoImage(file="./images/button_not.png")
+comments_not_btn = Button(image = counter_not_btn_img,
+                      borderwidth = 0,
+                      highlightthickness = 0,
+                      command = make_x,
+                      relief = "flat")
+comments_not_btn.place(x = 440, y = 185, width = 110, height = 90)
+comments_not_btn['command'] = lambda arg=comments_not_btn:make_x(arg)
 
 variables_text = Label(text = "Variables?",
                     bg = "#b98b97", fg = "#000000",
                     font = ("Georgia", int(37.0)))
 variables_text.place(x = 550.0, y = 300.0)
+variables_not_btn_img = PhotoImage(file="./images/button_not.png")
+variables_not_btn = Button(image = counter_not_btn_img,
+                      borderwidth = 0,
+                      highlightthickness = 0,
+                      command = make_x,
+                      relief = "flat")
+variables_not_btn.place(x = 440, y = 275, width = 110, height = 90)
+variables_not_btn['command'] = lambda arg=variables_not_btn:make_x(arg)
 
 select_file = Label(text = "select file",
                     bg = "#b98b97", fg = "#000000",
