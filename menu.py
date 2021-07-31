b2971563dc665b553af9d19cf01e890d61fda9d9
a-wyrm <86126872+a-wyrm@users.noreply.github.com>
2021-07-31 18:49:52 -0400

    Add files via upload

--- menu.py
+++ menu.py
@@ -33,20 +33,20 @@ def make_x(image, name):
 
 window = Tk()
 window.title("Olm")
-window.geometry("870x580")
+window.geometry("870x680")
 window.configure(bg = "#b98b97")
 
 
 canvas = Canvas(
     window,
     bg = "#ebebeb",
-    height = 580,
+    height = 680,
     width = 870,
     bd = 0,
     highlightthickness = 0,
     relief = "ridge")
 canvas.place(x = 0, y = 0)
-canvas.create_rectangle(435, 0, 435 + 435, 0 + 580, fill = "#b98b97", outline="")
+canvas.create_rectangle(435, 0, 435 + 435, 0 + 680, fill = "#b98b97", outline="")
 
 title = Label(text="Welcome to Olm!",
               bg = "#ebebeb", fg = "#b98b97",
@@ -76,18 +76,30 @@ instructions_text = Label(text="The first button will ask if you want the text\n
                   font = ("Georgia", int(13.5)))
 
 instructions_text.place(x = 27.0, y = 230.0)
+
+note_text = Label(text="Please note that larger files will take\n"
+                  "longer to process.",
+                  bg = "#ebebeb", fg = "#b98b97",
+                  justify = "left",
+                  font = ("Georgia", int(15.5)))
+
+note_text.place(x = 27.0, y = 500.0)
+
 trans_btn_img = PhotoImage(file="./images/trans_img.png")
 trans_btn = Button(image = trans_btn_img,
                       borderwidth = 0,
                       highlightthickness = 0,
                       command = trans_clicked,
                       relief = "flat")
-trans_btn.place(x = 30, y = 480, width = 380, height = 70)
+trans_btn.place(x = 30, y = 580, width = 380, height = 70)
+
+
 
 slow_flag = 0
 counter_flag = 0
 comments_flag = 0
 variables_flag = 0
+start_flag = 0
 
 img = PhotoImage(file="./images/button_not.png")
 img2 = PhotoImage(file="./images/button_true.png")
@@ -96,6 +108,7 @@ slow_image_flag = 0
 counter_image_flag = 0
 comments_image_flag = 0
 variables_image_flag = 0
+start_image_flag = 0
 
 slower_text = Label(text = "Slower text?",
                     bg = "#b98b97", fg = "#000000",
@@ -114,8 +127,7 @@ counter_text = Label(text = "Counter?",
                     bg = "#b98b97", fg = "#000000",
                     font = ("Georgia", int(37.0)))
 counter_text.place(x = 550.0, y = 120.0)
-counter_not_btn_img = img
-counter_not_btn = Button(image = counter_not_btn_img,
+counter_not_btn = Button(image = slow_not_btn_img,
                       borderwidth = 0,
                       highlightthickness = 0,
                       command = make_x,
@@ -129,8 +141,7 @@ comments_text = Label(text = "Comments?",
                     bg = "#b98b97", fg = "#000000",
                     font = ("Georgia", int(37.0)))
 comments_text.place(x = 550.0, y = 210.0)
-comments_not_btn_img = img
-comments_not_btn = Button(image = counter_not_btn_img,
+comments_not_btn = Button(image = slow_not_btn_img,
                       borderwidth = 0,
                       highlightthickness = 0,
                       command = make_x,
@@ -142,8 +153,7 @@ variables_text = Label(text = "Variables?",
                     bg = "#b98b97", fg = "#000000",
                     font = ("Georgia", int(37.0)))
 variables_text.place(x = 550.0, y = 300.0)
-variables_not_btn_img = img
-variables_not_btn = Button(image = counter_not_btn_img,
+variables_not_btn = Button(image = slow_not_btn_img,
                       borderwidth = 0,
                       highlightthickness = 0,
                       command = make_x,
@@ -151,14 +161,26 @@ variables_not_btn = Button(image = counter_not_btn_img,
 variables_not_btn.place(x = 440, y = 275, width = 110, height = 90)
 variables_not_btn['command'] = lambda arg = variables_not_btn:make_x(arg, "variables_not_btn")
 
+start_text = Label(text = "Start from where you\n"
+                   "last left off?",
+                    bg = "#b98b97", fg = "#000000",
+                    font = ("Georgia", int(23.0)))
+start_text.place(x = 550.0, y = 390.0)
+start_not_btn = Button(image = slow_not_btn_img,
+                      borderwidth = 0,
+                      highlightthickness = 0,
+                      command = make_x,
+                      relief = "flat")
+start_not_btn.place(x = 440, y = 365, width = 110, height = 90)
+start_not_btn['command'] = lambda arg = start_not_btn:make_x(arg, "start_not_btn")
 
 
 select_file = Label(text = "select file",
                     bg = "#b98b97", fg = "#000000",
                     font = ("Arial-BoldMT", int(25.0)))
-select_file.place(x = 460.0, y = 380.0)
+select_file.place(x = 460.0, y = 480.0)
 path_entry = Entry(bd = 0, bg = "#F6F7F9", highlightthickness = 0)
-path_entry.place(x = 460.0, y = 420, width = 380.0, height = 35)
+path_entry.place(x = 460.0, y = 520, width = 380.0, height = 35)
 path_entry.bind("<1>", select_path)
 
 generate_btn_img = PhotoImage(file="./images/generate.png")
@@ -167,7 +189,7 @@ generate_btn = Button(image = generate_btn_img,
                       highlightthickness = 0,
                       command = btn_clicked,
                       relief = "flat")
-generate_btn.place(x = 460, y = 470, width = 380, height = 70)
+generate_btn.place(x = 460, y = 570, width = 380, height = 70)
 
 window.resizable(False, False)
 window.mainloop()
