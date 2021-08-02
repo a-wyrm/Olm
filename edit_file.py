731b706b278d96b9928ce545a9ab151207dd4f2f
a-wyrm <86126872+a-wyrm@users.noreply.github.com>
2021-08-02 16:29:15 -0400

    Add files via upload

--- edit_file.py
+++ edit_file.py
@@ -11,10 +11,11 @@ edit_file = True
 # note: make it so temp_file is deleted first
 # rename have tts voice over the temp file if option is selected
 
+oc = 0
+tc = 0
+
 program_name = 'Olm_t.py'
 with open(program_name,'r') as tts_file, open('Olm_wf.txt', 'r') as Olm_f, open('temp_file.txt','a') as write_f:
-    for olm_line in Olm_f:
-        for line in tts_file:
-            if line != olm_line:
-                write_f.write(line)
-            break
+    same = set(tts_file).difference(Olm_f)
+    str_val = " ".join(map(str, same))
+    write_f.write(str_val)
