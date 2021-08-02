1738b4b898b58c8af65de9dac04f7cf2f31f3507
a-wyrm <86126872+a-wyrm@users.noreply.github.com>
2021-08-02 18:21:48 -0400

    Add files via upload

--- edit_file.py
+++ edit_file.py
@@ -1,4 +1,6 @@
 import os
+from difflib import Differ
+
 edit_file = True
 
 #if (os.path.isfile('Olm_wf.txt') == True) and edit_file == True:
@@ -11,11 +13,12 @@ edit_file = True
 # note: make it so temp_file is deleted first
 # rename have tts voice over the temp file if option is selected
 
-oc = 0
-tc = 0
 
 program_name = 'Olm_t.py'
-with open(program_name,'r') as tts_file, open('Olm_wf.txt', 'r') as Olm_f, open('temp_file.txt','a') as write_f:
-    same = set(tts_file).difference(Olm_f)
-    str_val = " ".join(map(str, same))
-    write_f.write(str_val)
+with open(program_name, 'r') as tts_file, open('Olm_wf.txt', 'r') as Olm_f, open('temp_file.txt','a') as write_f:
+    differ = Differ()
+
+    for line in differ.compare(tts_file.readlines(), Olm_f.readlines()):
+        if line.startswith("-"):
+            print(line[2:], end="")
+            break
